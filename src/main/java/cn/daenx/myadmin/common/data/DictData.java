8c271e854aeac31bed550199379be32ad6e986f9
DaenMax <1330166565@qq.com>
2024-10-30 13:36:49 +0800

    优化字典和菜单类逻辑

--- src/main/java/cn/daenx/myadmin/common/data/DictData.java
+++ src/main/java/cn/daenx/myadmin/common/data/DictData.java
@@ -58,55 +58,44 @@ public class DictData {
 
     public DictData() {
         data = new java.util.HashMap<>();
-        CommonYesNo();
-        CommonStatus();
-        CommonLock();
-        CommonNeed();
-        DemoPeopleSex();
-    }
-
-    private void CommonYesNo() {
-        //通用是否：0=否，1=是
-        String dictCode = "dict_common_yes_no";
+        String dictCode;
         List<DictDetailVo> list = new ArrayList<>();
+
+        //通用是否：0=否，1=是
+        dictCode = "dict_common_yes_no";
+        list.clear();
         list.add(new DictDetailVo(dictCode, "否", "0", DictClassConstant.SUCCESS, "1"));
         list.add(new DictDetailVo(dictCode, "是", "1", DictClassConstant.PRIMARY, "1"));
         this.data.put(dictCode, list);
-    }
-    private void CommonStatus() {
+
         //状态，0=正常，1=禁用
-        String dictCode = "dict_common_status";
-        List<DictDetailVo> list = new ArrayList<>();
+        dictCode = "dict_common_status";
+        list.clear();
         list.add(new DictDetailVo(dictCode, "正常", "0", DictClassConstant.SUCCESS, "1"));
         list.add(new DictDetailVo(dictCode, "禁用", "1", DictClassConstant.DANGER, "1"));
         this.data.put(dictCode, list);
-    }
-    private void CommonLock() {
+
         //开关，0=开启，1=关闭
-        String dictCode = "dict_common_lock";
-        List<DictDetailVo> list = new ArrayList<>();
+        dictCode = "dict_common_lock";
+        list.clear();
         list.add(new DictDetailVo(dictCode, "开启", "0", DictClassConstant.SUCCESS, "1"));
         list.add(new DictDetailVo(dictCode, "关闭", "1", DictClassConstant.DANGER, "1"));
         this.data.put(dictCode, list);
-    }
 
-    private void CommonNeed() {
         //需要， 0=需要, 1=不需要
-        String dictCode = "dict_common_need";
-        List<DictDetailVo> list = new ArrayList<>();
+        dictCode = "dict_common_need";
+        list.clear();
         list.add(new DictDetailVo(dictCode, "需要", "0", DictClassConstant.SUCCESS, "1"));
         list.add(new DictDetailVo(dictCode, "不需要", "1", DictClassConstant.DANGER, "1"));
         this.data.put(dictCode, list);
-    }
-    private void DemoPeopleSex() {
+
         //性别：1=男，0=女
-        String dictCode = "dict_demo_people_sex";
-        List<DictDetailVo> list = new ArrayList<>();
+        dictCode = "dict_demo_people_sex";
+        list.clear();
         list.add(new DictDetailVo(dictCode, "女", "0", DictClassConstant.PRIMARY, "1"));
         list.add(new DictDetailVo(dictCode, "男", "1", DictClassConstant.SUCCESS, "1"));
         this.data.put(dictCode, list);
     }
-
 
 
 }
