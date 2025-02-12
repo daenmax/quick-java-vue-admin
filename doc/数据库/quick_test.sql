b849cc357f31ad149e7c281d7cbbeea257e77547
DaenMax <1330166565@qq.com>
2025-02-12 22:41:24 +0800

    重构很多逻辑

--- doc/数据库/quick_test.sql
+++ doc/数据库/quick_test.sql
@@ -17,17 +17,19 @@ CREATE TABLE `demo_people`  (
   `age` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '年龄',
   `sex` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '性别：1=男，0=女',
   `expire_time` datetime(0) NULL DEFAULT NULL COMMENT '到期时间，留空则永久',
+  `status` varchar(2) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT '0' COMMENT '状态，0=正常，1=禁用',
   PRIMARY KEY (`id`) USING BTREE
 ) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci COMMENT = '测试人员' ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of demo_people
 -- ----------------------------
-INSERT INTO `demo_people` VALUES ('274f9dabe36c995b249a91678e0a7723', '2024-09-08 19:51:05', '2024-09-08 19:51:05', 0, NULL, '李四2', NULL, '0', NULL);
-INSERT INTO `demo_people` VALUES ('2c6fe7356233695519af7f2681516aaf', '2024-09-08 19:46:58', '2024-09-08 19:46:58', 0, '济南的', '张三', '18', '0', '2024-09-08 19:45:37');
-INSERT INTO `demo_people` VALUES ('64f25be2a305697d609f5021b9f74089', '2024-09-08 19:51:05', '2024-09-08 19:51:05', 0, '济南的', '张三1', NULL, '1', '2024-09-09 19:45:37');
-INSERT INTO `demo_people` VALUES ('eea92734fc31166ec5adef8224f75dd8', '2024-09-08 19:47:46', '2024-09-08 19:47:46', 0, NULL, '李四', NULL, '0', NULL);
-
+INSERT INTO `demo_people` VALUES ('274f9dabe36c995b249a91678e0a7723', '2024-09-08 19:51:05', '2025-02-12 22:36:13', 0, NULL, '李四2', NULL, '0', NULL, '1');
+INSERT INTO `demo_people` VALUES ('2c6fe7356233695519af7f2681516aaf', '2024-09-08 19:46:58', '2024-09-08 19:46:58', 0, '济南的', '张三', '18', '0', '2024-09-08 19:45:37', '0');
+INSERT INTO `demo_people` VALUES ('64f25be2a305697d609f5021b9f74089', '2024-09-08 19:51:05', '2025-02-12 22:36:04', 0, '济南的', '张三1', NULL, '1', '2024-09-09 19:45:37', '0');
+INSERT INTO `demo_people` VALUES ('c22b2b4325d391e214d21e37418e48b1', '2025-02-12 22:36:52', '2025-02-12 22:37:04', 0, NULL, '阿萨德', '23', '0', NULL, '1');
+INSERT INTO `demo_people` VALUES ('dbd0ca482667cc004d65bc42c5f2d3a5', '2025-02-12 22:37:39', '2025-02-12 22:37:39', 0, NULL, '你好', NULL, '0', NULL, '1');
+INSERT INTO `demo_people` VALUES ('eea92734fc31166ec5adef8224f75dd8', '2024-09-08 19:47:46', '2024-09-08 19:47:46', 0, NULL, '李四', NULL, '0', NULL, '0');
 -- ----------------------------
 -- Table structure for system_config
 -- ----------------------------
